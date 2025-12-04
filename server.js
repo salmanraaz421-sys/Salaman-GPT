9340adc3a4765c7d407f51792377ca82546425d4
Salman Khursheed <salmanraaz421@gmail.com>
2025-12-04 16:51:34 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Updated API Key
-const key = "825aff7f632189f2be98f735f0a0a23e";
+const key = "b0ba45bb2d0864dfe2fa95f792abe53f";
 const sdk = new Bytez(key);
 const model = sdk.model("google/imagen-4.0-generate-001");
 
